43860d71ba3425962795c61aebbd611027dccef5
ElSanteel <alsantel.ma@gmail.com>
2024-02-07 18:43:51 +0200

    created featured books state

--- lib/Features/Home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/Home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,4 @@
-import 'package:book_store_app/Features/home/presentation/views/widgets/best_seller_list_view.dart';
+import 'package:book_store_app/Features/home/presentation/views/widgets/newest_books_list_view.dart';
 import 'package:book_store_app/Features/home/presentation/views/widgets/custom_app_bar.dart';
 import 'package:book_store_app/Features/home/presentation/views/widgets/featured_list_view.dart';
 import 'package:book_store_app/core/utils/styles.dart';
@@ -34,7 +34,7 @@ class HomeViewBody extends StatelessWidget {
         SliverFillRemaining(
           child: Padding(
             padding: EdgeInsets.symmetric(horizontal: 30),
-            child: BestSellerListView(),
+            child: NewestBooksListView(),
           ),
         )
       ],
