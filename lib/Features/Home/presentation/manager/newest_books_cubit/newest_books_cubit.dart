b3ac7a8d85024ba21385261e74374bcde6173288
ElSanteel <alsantel.ma@gmail.com>
2024-02-07 18:59:47 +0200

    renaming fetchFeaturedBooks methods to fetchNewestBooks

--- lib/Features/Home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
+++ lib/Features/Home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -11,7 +11,7 @@ class NewestBooksCubit extends Cubit<NewestBooksState> {
 
     final HomeRepo homeRepo;
 
-  Future<void> fetchFeaturedBooks() async {
+  Future<void> fetchNewestBooks() async {
     emit(NewestBooksLoading());
     var result = await homeRepo.fetchFeaturedBooks();
     result.fold(
