b0078d22cfe5f15400bc263224dbe1b1464427d2
ElSanteel <alsantel.ma@gmail.com>
2024-02-07 20:13:38 +0200

    displayed featured books part 1

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,8 @@ import 'package:book_store_app/Features/home/data/repos/home_repo_implementation
 import 'package:book_store_app/Features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
 import 'package:book_store_app/Features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
 import 'package:book_store_app/constants.dart';
-import 'package:book_store_app/core/utils/api_service.dart';
 import 'package:book_store_app/core/utils/routes.dart';
 import 'package:book_store_app/core/utils/service_locator.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
