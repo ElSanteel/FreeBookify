5e83c750717915190c5c1717dd7fe75fd7e3168e
ElSanteel <alsantel.ma@gmail.com>
2024-02-07 17:17:44 +0200

    Implement fetch featured books method

--- lib/Features/Home/data/repos/home_repo_implementation.dart
+++ lib/Features/Home/data/repos/home_repo_implementation.dart
@@ -34,14 +34,31 @@ class HomeRepoImplementation extends HomeRepo {
         ),
       );
     }
-
-    // ignore: dead_code
   }
-  
+
   @override
-  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() {
-    // TODO: implement fetchFeaturedBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint: 'volumes?Filtering=free-ebooks&q=subject: Programming');
+      List<BookModel> books = [];
+      for (var item in data['items']) {
+        books.add(
+          BookModel.fromJson(item),
+        );
+      }
+      return right(books);
+    } catch (e) {
+      if (e is DioException) {
+        return left(
+          ServerFailure.fromDioException(e),
+        );
+      }
+      return left(
+        ServerFailure(
+          e.toString(),
+        ),
+      );
+    }
   }
-
 }
