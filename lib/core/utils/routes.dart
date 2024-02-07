877ce7f5cb463b9fbc925148db6fa140622d4a69
ElSanteel <alsantel.ma@gmail.com>
2024-02-07 03:43:41 +0200

    create search view and make searchView route

--- lib/core/utils/routes.dart
+++ lib/core/utils/routes.dart
@@ -2,11 +2,14 @@
 import 'package:book_store_app/Features/Home/presentation/views/book_details_view.dart';
 import 'package:book_store_app/Features/Home/presentation/views/home_view.dart';
 import 'package:book_store_app/Features/Splash/presentation/views/splash_view.dart';
+import 'package:book_store_app/Features/search/presentation/views/search_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kBookDetailsView = '/bookDetailsView';
+  static const kSearchView = '/searchView';
+
 
   static final router = GoRouter(
     routes: [
@@ -21,6 +24,10 @@ abstract class AppRouter {
       GoRoute(
         path: kBookDetailsView,
         builder: (context, state) => const BookDetailsView(),
+      ),
+      GoRoute(
+        path: kSearchView,
+        builder: (context, state) => const SearchView(),
       ),
     ],
   );
